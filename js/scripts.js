c377e6075992898f9174fa2445e7ab8310afb567
Dominic Mugo <njuemugodominic@gmail.com>
2020-10-04 21:40:42 +0300

    create an array for the days of the week

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ function checkDayOfWeek() {
     var dateObject = new Date(dateString)
     var gender = document.querySelector('input[name="gender"]:checked').value;
     var weekday = new Array(7);
+
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
